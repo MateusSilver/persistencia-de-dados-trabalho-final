b3dc72fa37dfd44b4687ef7d2e6972a7df2f345e
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2024-11-17 15:22:40 -0300

    feat: update models

--- src/main/java/model/agenda.java
+++ src/main/java/model/agenda.java
@@ -32,4 +32,68 @@ public class agenda {
 		
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public LocalTime getHora() {
+		return hora;
+	}
+
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+	public LocalDate getData_situacao() {
+		return data_situacao;
+	}
+
+	public void setData_situacao(LocalDate data_situacao) {
+		this.data_situacao = data_situacao;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public usuario getUsuario_id() {
+		return usuario_id;
+	}
+
+	public void setUsuario_id(usuario usuario_id) {
+		this.usuario_id = usuario_id;
+	}
+
+	public vacina getVacina_id() {
+		return vacina_id;
+	}
+
+	public void setVacina_id(vacina vacina_id) {
+		this.vacina_id = vacina_id;
+	}
+	
 }
