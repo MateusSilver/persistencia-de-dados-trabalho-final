f57d9ae7368a43208c737222a5560f9bb4b97d10
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2024-12-11 10:41:25 -0300

    feat: add index with chooses

---
+++ src/main/java/model/index.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.Scanner;
+
+class index {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.err.println("AGENDA DE VACINAÇÃO\n\n1. Exibir todas as vacinas\n2. Cadastrar uma nova vacina\n3. Sair");
+
+        int entrada = sc.nextInt();
+        sc.nextLine();
+        while(entrada != 3){
+            switch (entrada) {
+                case (1):
+                    // exibeVacinas()
+                    break;
+                case (2):
+                    // cadastrarVacina()
+                    break;
+                default:
+                    System.out.println("insira um valor valido\n");
+                    break;
+            }
+            entrada = sc.nextInt();
+            sc.nextLine();
+        }
+    }
+}
